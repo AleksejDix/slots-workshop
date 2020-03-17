06b76e2fc620a0e38e781ab41406059b34ba59b9
Aleksej <git@aleksejdix.com>
2020-03-17 19:25:56 +0100

    conditional styling

---
+++ src/views/Lessons/7/components/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="button">
+    <slot></slot>
+    <div class="second" v-if="$slots.second">
+      <slot name="second"></slot>
+    </div>
+  </button>
+</template>
+
+<style>
+.button {
+  color: white;
+  align-items: center;
+  padding: 0.375rem 0.75rem;
+  min-height: 40px;
+  border: none;
+  background-color: #41b883;
+  border-radius: 0.25rem;
+  padding: 0.5 1rem;
+  font-weight: bold;
+  letter-spacing: 0.5px;
+  font-size: 1.5rem;
+  line-height: 1.125;
+}
+
+.second {
+  display: block;
+  font-size: 12px;
+  min-height: 20px;
+  margin-top: 0.5rem;
+  border-top: 2px solid white;
+  padding-top: 0.5rem;
+}
+</style>
---
+++ src/views/Lessons/7/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Conditional wrapper</h1>
+    <AppButton>
+      buy <strong>now</strong>
+      <!-- <template #second>
+        only 3 Tickets left
+      </template> -->
+    </AppButton>
+  </div>
+</template>
+
+<script>
+import AppButton from "./components/AppButton";
+export default {
+  components: {
+    AppButton
+  }
+};
+</script>
